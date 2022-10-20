08eae4923ad113eb84b7bd72abed66394955ed9f
LP <lp.passarelli.wilson@gmail.com>
2022-10-20 14:31:03 -0700

    got it working to display hello world in postman:

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,8 @@
 from flask import Blueprint
 
 hello_world_bp = Blueprint("hello_world", __name__)
+
+@hello_world_bp.route("/hello-world", methods = ["GET"])
+def say_hello_world():
+    my_response_body = "Hello World!"
+    return my_response_body
