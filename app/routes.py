acd1098b6fe693710927cd5835b324f558aaec05
LP <lp.passarelli.wilson@gmail.com>
2022-10-21 10:05:31 -0700

    took out a redundant loop

--- app/routes.py
+++ app/routes.py
@@ -33,12 +33,10 @@ def get_all_books():
 def get_one_book(book_id):
     book = validate_book(book_id)
     book_id = int(book_id)
-    for book in books:
-        if book.id == book_id:
-            return {
-                "id" : book.id, 
-                "title" : book.title,
-                "description" : book.description
+    return {
+            "id" : book.id, 
+            "title" : book.title,
+            "description" : book.description
             }
 
 def validate_book(book_id):
